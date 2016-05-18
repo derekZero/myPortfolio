86d21767e54c59a9a8df84c6dd6dcfd66227c0fb
derekZero <derek.guzman777@gmail.com>
2016-05-18 14:27:26 -0500

    Javascript if statements used in corroboration with CSS media queries centered effects across all screen sizes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,73 @@
 $(document).ready(function() {
 	$(".about").animate({
-		top: "10vh"
-	}, 1000, function() {});
+			top: "10vh"
+		}, 1000, function() {});
 	$(".work").animate({
-		bottom: "10vh"
-	}, 1000, function() {});
-	$(".titleOne").animate({
-		opacity: 1,
-		right: "38vw"
-	}, 1000, function() {});
-	$(".slash").animate({
-		opacity: 1
-	}, 1000, function() {});
-	$(".titleTwo").animate({
-		opacity: 1,
-		right: "38vw"
-	}, 1000, function() {});
+			bottom: "10vh"
+		}, 1000, function() {});
+	if($(window).width() > 768){
+		$(".titleOne").animate({
+			opacity: 1,
+			right: "40vw"
+		}, 1000, function() {});
+		$(".slash").animate({
+			opacity: 1
+		}, 1000, function() {});
+		$(".titleTwo").animate({
+			opacity: 1,
+			right: "40vw"
+		}, 1000, function() {});
+	}
+	if($(window).width() <=768 && $(window).width() > 425){
+		$(".titleOne").animate({
+			opacity: 1,
+			right: "31vw"
+		}, 1000, function() {});
+		$(".slash").animate({
+			opacity: 1
+		}, 1000, function() {});
+		$(".titleTwo").animate({
+			opacity: 1,
+			right: "31vw"
+		}, 1000, function() {});
+	}
+	if($(window).width() <= 425 && $(window).width() > 375){
+		$(".titleOne").animate({
+			opacity: 1,
+			right: "19vw"
+		}, 1000, function() {});
+		$(".slash").animate({
+			opacity: 1
+		}, 1000, function() {});
+		$(".titleTwo").animate({
+			opacity: 1,
+			right: "19vw"
+		}, 1000, function() {});
+	}
+	if($(window).width() <= 375 && $(window).width() > 320){
+		$(".titleOne").animate({
+			opacity: 1,
+			right: "28vw"
+		}, 1000, function() {});
+		$(".slash").animate({
+			opacity: 1
+		}, 1000, function() {});
+		$(".titleTwo").animate({
+			opacity: 1,
+			right: "28vw"
+		}, 1000, function() {});
+	}
+	if($(window).width() <=320){
+		$(".titleOne").animate({
+			opacity: 1,
+			right: "28vw"
+		}, 1000, function() {});
+		$(".slash").animate({
+			opacity: 1
+		}, 1000, function() {});
+		$(".titleTwo").animate({
+			opacity: 1,
+			right: "28vw"
+		}, 1000, function() {});
+	}
 });

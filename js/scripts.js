ffb022d0d653e76a7a47c4b171c8181722e496b0
derekZero <derek.guzman777@gmail.com>
2016-05-18 01:51:05 -0500

    Initial homepage mostly done. Javascript needs responsivity to smaller screens.

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	$(".about").animate({
+		top: "10vh"
+	}, 1000, function() {});
+	$(".work").animate({
+		bottom: "10vh"
+	}, 1000, function() {});
+	$(".titleOne").animate({
+		opacity: 1,
+		right: "38vw"
+	}, 1000, function() {});
+	$(".slash").animate({
+		opacity: 1
+	}, 1000, function() {});
+	$(".titleTwo").animate({
+		opacity: 1,
+		right: "38vw"
+	}, 1000, function() {});
+});
